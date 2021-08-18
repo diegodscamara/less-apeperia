fdd2731ca7c6aafe7f38b87d988a054b13cf3054
Your Namegit <you@example.com>
2021-08-18 15:00:02 -0300

    nesting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,22 @@
 module.exports = function(grunt) {
-  require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt);
 
-  grunt.initConfig({
-    less: {
-      development: {
-        files: {
-          "css/estilos.css": "css/estilos.less" // Caminho dos arquivos
+    grunt.initConfig({
+        less: {
+            development: {
+                files: {
+                    "css/estilos.css": "css/estilos.less", // Caminho dos arquivos
+                    "css/media-queries.css": "css/media-queries.less", // Caminho dos arquivos
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['**/*.less'], // Quais arquivos o grunt ficará de olho
+                tasks: ['less']
+            }
         }
-      }
-    },
-    watch: {
-      styles: {
-        files: ['**/*.less'], // Quais arquivos o grunt ficará de olho
-        tasks: ['less']
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('default', ['less', 'watch']);
+    grunt.registerTask('default', ['less', 'watch']);
 };
